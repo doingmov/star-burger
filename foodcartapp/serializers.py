45d809456a9aa1920876d32b067fbd0164fcc100
doingmov <yohei13111@mail.ru>
2026-08-09 21:20:37 +0300

    Setup DRF

---
+++ foodcartapp/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Order, OrderItem
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    products = OrderItemSerializer(many=True, allow_empty=False, write_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['firstname', 'lastname', 'phonenumber', 'address', 'products']
--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -2,9 +2,11 @@ import json
 
 from django.http import JsonResponse
 from django.templatetags.static import static
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Product, Order, OrderItem
+from .serializers import OrderSerializer
 
 
 def banners_list_api(request):
@@ -59,21 +61,23 @@ def product_list_api(request):
     })
 
 
+@api_view(['POST'])
 def register_order(request):
-    data = json.loads(request.body.decode('utf-8'))
+    serializer = OrderSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
 
     order = Order.objects.create(
-        firstname=data['firstname'],
-        lastname=data['lastname'],
-        phonenumber=data['phonenumber'],
-        address=data['address'],
+        firstname=serializer.validated_data['firstname'],
+        lastname=serializer.validated_data['lastname'],
+        phonenumber=serializer.validated_data['phonenumber'],
+        address=serializer.validated_data['address'],
     )
 
-    for item in data['products']:
+    for item in serializer.validated_data['products']:
         OrderItem.objects.create(
             order=order,
-            product=Product.objects.get(id=item['product']),
+            product=item['product'],
             quantity=item['quantity'],
         )
 
-    return JsonResponse({})
+    return Response(OrderSerializer(order).data)
